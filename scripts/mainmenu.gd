d427432b7cfcb130b7c1be31c19b714e8a07f545
Leif <81562562+Deskilling@users.noreply.github.com>
2024-06-07 21:08:26 +0200

    Intial Commit
    - Main Menu
    - Impulses

---
+++ scripts/mainmenu.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _scene(scene):
+	get_tree().change_scene_to_file("res://scenes/"+ scene + ".tscn")
+
+func _on_to_impulse_pressed():
+	_scene("impulse")
+
+func _on_to_rollen_orte_konflikte_pressed():
+	pass # Replace with function body.
+
+func _on_to_stimmungen_pressed():
+	pass # Replace with function body.
+
+func _on_to_emojis_pressed():
+	pass # Replace with function body.
+ 
