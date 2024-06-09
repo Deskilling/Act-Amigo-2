e7bcf6cab267aa0aedd6f82c8aa5d6c938d13b8c
OmegaBaum <finnkitschke@gmail.com>
2024-06-09 16:03:15 +0200

    Add Rarity to gambling

--- src/scripts/item_chain.gd
+++ src/scripts/item_chain.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-var itemcontainer = preload("res://scenes/impulse/item_container.tscn")
+var itemcontainer_consumer = preload("res://scenes/impulse/item_container_consumer.tscn")
+var itemcontainer_industrial = preload("res://scenes/impulse/item_container_industrial.tscn")
+var itemcontainer_milspec = preload("res://scenes/impulse/item_container_milspec.tscn")
+var itemcontainer_restricted = preload("res://scenes/impulse/item_container_restricted.tscn")
+var itemcontainer_classified = preload("res://scenes/impulse/item_container_classified.tscn")
+var itemcontainer_covert = preload("res://scenes/impulse/item_container_covert.tscn")
+var itemcontainer_contraband =preload("res://scenes/impulse/item_container_contraband.tscn")
 var item_chain = []
 
 var spin_duration = 5.0
@@ -18,7 +24,24 @@ func _ready():
 func generate_chain():
 	var speed = (randi() % 5000 + 5000)
 	for i in range(0, CHAIN_SIZE):
-		var item_container_instance = itemcontainer.instantiate()
+		var rng = RandomNumberGenerator.new()
+		var item_container_instance = null
+		var rng_number = rng.randf_range(0.0, 100.0)
+		if rng_number < 0.26:
+			item_container_instance = itemcontainer_contraband.instantiate()
+		elif rng_number < 0.9:
+			item_container_instance = itemcontainer_covert.instantiate()
+		elif rng_number < 4.1:
+			item_container_instance = itemcontainer_classified.instantiate()
+		elif rng_number < 20:
+			item_container_instance = itemcontainer_restricted.instantiate()
+		elif rng_number < 35:
+			item_container_instance = itemcontainer_milspec.instantiate()
+		elif rng_number < 70:
+			item_container_instance = itemcontainer_industrial.instantiate()
+		else:
+			item_container_instance = itemcontainer_consumer.instantiate()
+		
 		add_child(item_container_instance)
 		item_chain.append(item_container_instance)
 		item_container_instance.index = i
